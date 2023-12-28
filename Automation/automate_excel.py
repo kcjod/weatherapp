7387fcf3bc18d2fab939ce52d4a78cf11a73c6f9
kcjod <146459126+kcjod@users.noreply.github.com>
2023-12-28 21:03:56 +0530

    Delete Automation directory

--- Automation/automate_excel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Using a relative path if the Excel file is in the same directory
-df = pd.read_excel('file.xlsx')
-
-# OR using the full path
-# df = pd.read_excel('/full/path/to/file.xlsx')
-
-print(df)
